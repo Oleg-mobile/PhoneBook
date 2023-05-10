da7918ec0d55bd111f382fb47ca39939f73e83ee
Oleg Ivanov <juklino@yandex.ru>
2023-05-10 13:03:15 +0300

    Добавьте файлы проекта.

---
+++ PhoneBook.WebApi/Controllers/ContactsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhoneBook.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactsController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult GetAll2()
+        {
+            return Ok();
+        }
+    }
+}
